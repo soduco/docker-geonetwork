01b310dc8a4f9eba374138c61f48d624fadc1b49
mhersent <melvin.hersent@gmail.com>
2022-02-21 15:43:44 +0100

    cascade delete added

--- 4.0.5/restore_db.sh
+++ 4.0.5/restore_db.sh
@@ -3,7 +3,7 @@
 # We need to drop the db first or default value won't be updated. 
 # We can't drop the DB since it is accessed by other processes (tried to kill other containers, there was still processes)
 # So we drop "public schema" and recreat it
-docker exec -i 405_database_1 psql -U geonetwork -d geonetwork -c "DROP SCHEMA public;"
+docker exec -i 405_database_1 psql -U geonetwork -d geonetwork -c "DROP SCHEMA public CASCADE;"
 docker exec -i 405_database_1 psql -U geonetwork -d geonetwork -c "CREATE SCHEMA public;"
 
 # To update. Work for now since there is only one dump
