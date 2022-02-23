f7a85549bb383d960be87f999b6738bf2fc01feb
mhersent <melvin.hersent@gmail.com>
2022-02-23 16:53:31 +0100

    path for sql dump updated

--- 4.0.5/backup_db.sh
+++ 4.0.5/backup_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -t 405_database_1 pg_dumpall -c -U geonetwork > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+docker exec -t 405_database_1 pg_dumpall -c -U geonetwork > sql_dumps/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
