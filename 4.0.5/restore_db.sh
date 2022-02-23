fd62f261ca6779db824b78e227efbebf6055aa58
mhersent <melvin.hersent@gmail.com>
2022-02-23 16:37:14 +0100

    script now need to choose a sql dump dumps deplaced in a folder

--- 4.0.5/restore_db.sh
+++ 4.0.5/restore_db.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# check if there is a paramater
+if [[ $# -eq 0 ]] ; then
+    echo 'please choose the SQL dump you want to restore'
+    exit 0
+fi
+
 # We need to drop the db first or default value won't be updated. 
 # We can't drop the DB since it is accessed by other processes (tried to kill other containers, there was still processes)
 # So we drop "public schema" and recreat it
@@ -7,4 +13,4 @@ docker exec -i 405_database_1 psql -U geonetwork -d geonetwork -c "DROP SCHEMA p
 docker exec -i 405_database_1 psql -U geonetwork -d geonetwork -c "CREATE SCHEMA public;"
 
 # To update. Work for now since there is only one dump
-cat dump_11-02-2022_16_26_40.sql | docker exec -i 405_database_1 psql -U geonetwork -d geonetwork
+cat "$1" | docker exec -i 405_database_1 psql -U geonetwork -d geonetwork
