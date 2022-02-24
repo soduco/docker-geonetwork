66c6d27d484965536076d01a4b34d0bed7a4be42
mhersent <melvin.hersent@gmail.com>
2022-02-24 10:33:47 +0100

    backup files added to gitignore restore: other volumes than geonetwork commented

--- 4.0.5/restore_volumes.sh
+++ 4.0.5/restore_volumes.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # geonetwork data
-docker run --rm --volumes-from 405_geonetwork_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /catalogue-data && tar xvf /backup/geonetwork.tar"
+docker run --rm --volumes-from 405_geonetwork_1 -v $(pwd)/backup:/backup ubuntu bash -c "tar xvf /backup/geonetwork.tar"
 # es data
-docker run --rm --volumes-from 405_elasticsearch_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /usr/share/elasticsearch/data && tar xvf /backup/esdata.tar"
+# docker run --rm --volumes-from 405_elasticsearch_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /usr/share/elasticsearch/data && tar xvf /backup/esdata.tar"
 #es snapshots
-docker run --rm --volumes-from 405_elasticsearch_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /usr/share/elasticsearch/backup && tar xvf /backup/esbackup.tar"
+# docker run --rm --volumes-from 405_elasticsearch_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /usr/share/elasticsearch/backup && tar xvf /backup/esbackup.tar"
 # db data
-docker run --rm --volumes-from 405_database_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /var/lib/postgresql/data && tar xvf /backup/pgdata.tar"
+# docker run --rm --volumes-from 405_database_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /var/lib/postgresql/data && tar xvf /backup/pgdata.tar"
 # db log
-docker run --rm --volumes-from 405_database_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /var/log/postgresql && tar xvf /backup/pglog.tar"
+# docker run --rm --volumes-from 405_database_1 -v $(pwd)/backup:/backup ubuntu bash -c "cd /var/log/postgresql && tar xvf /backup/pglog.tar"
